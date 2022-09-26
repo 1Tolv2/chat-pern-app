7272845275630dba12c1505a56246fb8ce9cf0eb
1Tolv2 <sofiaq1@gmail.com>
2022-09-26 21:03:02 +0200

    Removed unused route

--- api/routes/posts.ts
+++ api/routes/posts.ts
@@ -1,10 +1,9 @@
 import express, { Router } from 'express'
-import { handleNewPost, getAllPostsByChannelId, getAllPosts} from '../controllers/posts'
+import { handleNewPost, getAllPosts} from '../controllers/posts'
 
 const router: Router = express.Router()
 
 router.get("/", getAllPosts)
 router.post("/", handleNewPost)
-router.get("/:id", getAllPostsByChannelId)
 
 export default router
