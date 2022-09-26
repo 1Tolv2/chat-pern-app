ea76a671b1513d634b22fb28f4d17dafaf7f52b7
1Tolv2 <sofiaq1@gmail.com>
2022-09-26 23:41:26 +0200

    Now fetches messages

--- client/src/components/organisms/MessageList/index.tsx
+++ client/src/components/organisms/MessageList/index.tsx
@@ -1,40 +1,35 @@
 import React, { useEffect } from "react";
-import MessageItem from "../../molecules/MessageItem";
 import { Post } from "../../../global/types";
-import { getChannelWithPosts } from '../../../global/api';
+import { getChannelPosts } from "../../../global/api";
+import MessageItem from "../../molecules/MessageItem";
 import * as s from "./styles";
 
 type Props = {};
 
-// const messageList: Message[] = [
-//   { id: 1, username: "tolv", text: "Hello World", timeStamp: new Date() },
-//   { id: 2, username: "test", text: "Hello World2", timeStamp: new Date() },
-// ];
-
 const MessageList = (props: Props) => {
-const [posts, setPosts] = React.useState<Post[]>([]);
+  const [posts, setPosts] = React.useState<Post[]>([]);
 
-const fetchData = async () => {
-  try {
-  const data = await getChannelWithPosts("6c5b4071-7f0f-4f47-b5a7-ae74d6ca06cf")
-  setPosts(data.posts)
-  } catch(err) {
-    console.log(err)
-    setPosts([])
-    console.log("Something went wrong when fetching my posts...")
-  }
-}
+  const fetchData = async () => {
+    try {
+      const data = await getChannelPosts(
+        "6c5b4071-7f0f-4f47-b5a7-ae74d6ca06cf"
+      );
+      setPosts(data.posts);
+    } catch (err) {
+      console.log(err);
+      setPosts([]);
+      console.log("Something went wrong when fetching my posts...");
+    }
+  };
 
-useEffect(()=> {
-  fetchData()
-}, [])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <s.MessageList>
       {posts.map((post: Post, index) => {
-        return (
-          <MessageItem key={index} data={post}/>
-        );
+        return <MessageItem key={index} data={post} />;
       })}
     </s.MessageList>
   );
--- client/src/global/api.ts
+++ client/src/global/api.ts
@@ -1,23 +1,23 @@
-import axios from 'axios'
-import {Post, ActivityData} from './types'
+import axios from "axios";
+import { Post, ActivityData } from "./types";
 
-axios.defaults.baseURL = 'http://localhost:8800'
+axios.defaults.baseURL = "http://localhost:8800";
 
 export type Channel = {
-    id: string,
-    name: string,
-    description: string,
-    created_at: Date,
-    updated_at: Date,
-    posts: Post[];
-  }
+  id: string;
+  name: string;
+  description: string;
+  created_at: Date;
+  updated_at: Date;
+  posts: Post[];
+};
 
 export const getAllUsers = async () => {
-const res = await axios.get("/users")
-return res
-}
+  const res = await axios.get("/users");
+  return res;
+};
 
-export const getChannelWithPosts = async (channelId: string): Promise<Channel> => {
-    const res = await axios.get<Channel>(`/channels/${channelId}/posts`)
-    return res.data
-}
+export const getChannelPosts = async (channelId: string): Promise<Channel> => {
+  const res = await axios.get<Channel>(`/channels/${channelId}/posts`);
+  return res.data;
+};
