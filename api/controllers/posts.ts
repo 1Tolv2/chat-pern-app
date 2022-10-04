fb89814e9e5660155165929d40cb78ec45f562e4
1Tolv2 <sofiaq1@gmail.com>
2022-10-04 22:03:30 +0200

    Setup functions to posts

--- api/controllers/posts.ts
+++ api/controllers/posts.ts
@@ -1,25 +1,41 @@
 import { Request, Response } from "express";
+import { createPost } from "../models/Post";
 
-export const handleNewPost = async (req: Request, res: Response) => {
-  const {channelId, body} = req.body; 
-  console.log(req.user);
-  console.log(channelId, body);
-  
-  res.json({ post: { message: "New post created" } });
+export const handleNewPost = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { channelId, text } = req.body;
+  const post = await createPost({
+    user_id: parseInt(req.user?.userId),
+    channel_id: parseInt(channelId),
+    text,
+  });
+
+  res.json(post);
 };
 
-export const getAllPosts = async (req: Request, res: Response) => {
+export const getAllPosts = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   res.json({ posts: [{ message: "All posts" }] });
 };
 
-export const getPostById = async (req: Request, res: Response) => {
+export const getPostById = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   res.json({ post: [{ message: "Post with user and channel" }] });
 }; // get /:id with posts
 
-export const editPost = async (req: Request, res: Response) => {
+export const editPost = async (req: Request, res: Response): Promise<void> => {
   res.json({ post: { message: "Post updated" } });
 };
 
-export const deletePost = async (req: Request, res: Response) => {
+export const deletePost = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   res.json({ message: "Post deleted" });
 };
