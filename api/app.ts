38460912c7d57ad4a43269d737883bff0703c4fd
1Tolv2 <sofiaq1@gmail.com>
2022-10-05 12:13:33 +0200

    removed non functional error messages

--- api/app.ts
+++ api/app.ts
@@ -1,30 +1,36 @@
-import express, { Request, Response, Express, json, NextFunction } from "express";
+import express, {
+  Request,
+  Response,
+  Express,
+  json,
+  NextFunction,
+} from "express";
 import cors from "cors";
 import { PORT, ALTER_DATABASE } from "./config/config";
 import routes from "./routes/index";
-import jwt, {JwtPayload} from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 
 const app: Express = express();
 app.use(cors());
 app.use(json());
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use( async (req: Request, res: Response, next: NextFunction) => {
-  const token = req.cookies.access_token
+app.use(async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.cookies.access_token;
   if (token) {
-      try {
-        req.user = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
-      } catch (error: any) {
-        error.message === "jwt expired"
-          ? res.status(401).json({ error: "Token expired" })
-          : error.message === "invalid token"
-          ? res.status(401).json({ error: "Invalid token" })
-          : res.status(400).json({ error: "Token error" });
-      }
+    try {
+      req.user = jwt.verify(
+        token,
+        process.env.JWT_SECRET as string
+      ) as JwtPayload;
+    } catch (error: any) {
+      error.message === "invalid token" &&
+        res.status(401).json({ error: "Invalid token" });
+    }
   }
   next();
-})
+});
 
 app.use("/", routes);
 
