3f949b3a32338b29092af4668cfc4c6586340020
1Tolv2 <sofiaq1@gmail.com>
2022-10-13 14:55:44 +0200

    fixed width

--- packages/client/src/components/organisms/ServerList/styles.ts
+++ packages/client/src/components/organisms/ServerList/styles.ts
@@ -6,7 +6,7 @@ const { colors } = theme;
 const Container = styled.div`
   display: flex;
   justify-content: center;
-  width: 100%;
+  width: 72px;
   text-align: center;
   & > * {
     width: 48px;
