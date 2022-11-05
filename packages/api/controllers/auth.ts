5b2bdb7532013f219a0b44f8f1505193fd816072
1Tolv2 <sofiaq1@gmail.com>
2022-11-05 15:17:26 +0100

    added loginuser controller tests

--- packages/api/controllers/auth.ts
+++ packages/api/controllers/auth.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from "express";
-import User from "../models/User";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import dotenv from "dotenv";
-import { requiredFieldsCheck } from ".";
 import { UserItem } from "@chat-app-typescript/shared";
+import User from "../models/User";
+import { requiredFieldsCheck } from ".";
 dotenv.config();
 
 export const requireLogin = (
@@ -30,9 +30,11 @@ export const handleToken = async (
       req.user = verifyToken(jwt);
     } catch (err) {
       if (err instanceof Error) {
-        err.message === "invalid token" &&
-          res.status(401).json({ error: "Invalid token" });
-      } else console.error(err);
+        if (err.message === "invalid token") {
+          res.status(401);
+          res.json({ error: "Invalid token" });
+        }
+      }
     }
   }
   next();
@@ -40,7 +42,6 @@ export const handleToken = async (
 
 export const logInUser = async (req: Request, res: Response): Promise<void> => {
   const missingFields = requiredFieldsCheck(req.body, ["username", "password"]);
-
   if (missingFields.length === 0) {
     const username = req.body.username.toLowerCase();
     let user: UserItem | null = null;
@@ -49,26 +50,27 @@ export const logInUser = async (req: Request, res: Response): Promise<void> => {
       user = await User.authorizeUser(username, req.body.password);
     } catch (err) {
       if (err instanceof Error) {
-        console.error(err);
-        res.status(500).json({ error: err.message });
+        res.status(500);
+        res.json({ error: err.message });
       }
     }
 
     if (user) {
       const token = jwt.sign(
-        { user_id: user.id?.toString(), username: username },
+        { user_id: user.id?.toString(), username },
         process.env.JWT_SECRET as string,
         {
           expiresIn: "2h",
           subject: user.id?.toString(),
         }
       );
-      res
-        .cookie("jwt", token, { maxAge: 7200000, httpOnly: true })
-        .json({ user: { id: user.id?.toString(), username: username } });
+
+      res.cookie("jwt", token, { maxAge: 7200000, httpOnly: true });
+      res.json({ user: { id: user.id?.toString(), username } });
     }
   } else {
-    res.status(400).json({
+    res.status(400);
+    res.json({
       error: "Missing required fields",
       missingFields,
     });
---
+++ packages/api/controllers/tests/auth.test.ts
@@ -0,0 +1,76 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { NotFoundError, UniqueIntegrityConstraintViolationError } from "slonik";
+import User from "../../models/User";
+import * as auth from "../auth";
+
+let mockRequest: Partial<Request>;
+let mockResponse: Partial<Response>;
+let mockNext: NextFunction;
+
+beforeEach(() => {
+  mockRequest = {};
+  mockResponse = {
+    json: jest.fn(),
+    status: jest.fn(),
+    cookie: jest.fn(),
+  };
+  mockNext = jest.fn();
+});
+
+jest.mock("../../models/User", () => {
+  return {
+    __esModule: true,
+    ...jest.requireActual("../../models/User"),
+    findUserById: () =>
+      Promise.resolve({
+        id: "bc426c44-75d7-46fe-99f9-10793ed1adbb",
+        username: "tolv",
+        email: "",
+      }),
+  };
+});
+
+describe("Testing auth controllers", () => {
+  describe("Handling Login", () => {
+    test("Should call .cookie() with token and .json() with user information", async () => {
+      User.authorizeUser = jest.fn().mockResolvedValue({
+        id: "f4b9569b-76ea-49c0-ad55-a7f6c6213cf6",
+        username: "test",
+        email: "test@test.com",
+        created_at: 1667644130914,
+        updated_at: null,
+      });
+      jest.spyOn(jwt, "sign").mockImplementation(() => "token");
+
+      mockRequest.body = {
+        username: "Tolv",
+        password: "1234",
+      };
+
+      await auth.logInUser(mockRequest as Request, mockResponse as Response);
+
+      expect(mockResponse.cookie).toHaveBeenCalledWith("jwt", "token", {
+        maxAge: 7200000,
+        httpOnly: true,
+      });
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        user: { id: expect.any(String), username: "tolv" },
+      });
+    });
+
+    test("Should call .status() with 400 and .json() with an array of fields when missing required fields", async () => {
+      mockRequest.body = {
+        username: "Tolv",
+      };
+
+      await auth.logInUser(mockRequest as Request, mockResponse as Response);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(400);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        error: "Missing required fields",
+        missingFields: ["password"],
+      });
+    });
+  });
+});
